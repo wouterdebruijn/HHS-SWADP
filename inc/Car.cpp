6125f26930554ae9298c0543b88d8105f83bd7e6
Wouter de Bruijn <wouter@hedium.nl>
2022-05-19 17:12:14 +0200

    :sparkles: Finished Assignment 3

--- inc/Car.cpp
+++
@@ -1,5 +0,0 @@
-#include "Car.h"
-#include "Speed.h"
-
-Car::Car(Speed *speed) : speed(speed){};
-void Car::measureSpeed() { speed->measure(); };
--- main.cpp
+++ main.cpp
@@ -1,17 +1,108 @@
-#include "Speed.h"
-#include "Car.h"
+// ================================================================
+// Infrastructure Layer
+// ================================================================
+#include <iostream>
+#include <list>
+using namespace std;
+class Subject;
+class Observer
+{
+private:
+    Subject *S;
 
-#include "DigitalMeter.h"
-#include "AnalogMeter.h"
+protected:
+    Subject *getSubject() const { return S; }
 
+public:
+    Observer(Subject *s);
+    virtual ~Observer();
+    virtual void update() = 0;
+};
+class Subject
+{
+private:
+    list<Observer *> L;
+
+protected:
+    virtual void notify();
+
+public:
+    Subject() {}
+    virtual ~Subject() {}
+    virtual void insert(Observer *s) { L.push_front(s); }
+    virtual void remove(Observer *s) { L.remove(s); }
+};
+void Subject::notify()
+{
+    for (list<Observer *>::iterator i = L.begin(); i != L.end(); ++i)
+        (*i)->update();
+}
+Observer::Observer(Subject *s) : S(s)
+{
+    getSubject()->insert(this);
+}
+Observer::~Observer()
+{
+    getSubject()->remove(this);
+}
+
+// ================================================================
+// DomainLayer
+// ================================================================
+class Element : public Subject
+{
+public:
+    virtual ~Element() {}
+    virtual void draw() = 0;
+    virtual string name() = 0;
+};
+class Ninja : public Element
+{
+private:
+    string Name;
+    int AantalLevens;
+
+public:
+    Ninja(string n) : Name(n), AantalLevens(1) {}
+    virtual void draw() { cout << name() << AantalLevens; }
+    virtual string name() { return Name; }
+    virtual int aantalLevens() { return AantalLevens; }
+    virtual void incLevens()
+    {
+        AantalLevens++;
+        notify();
+    }
+    virtual void decLevens()
+    {
+        AantalLevens--;
+        notify();
+    }
+};
+
+// ================================================================
+// Interfacelayer
+// ================================================================
+class NinjaWindow : public Observer
+{
+private:
+    Ninja &N;
+
+public:
+    NinjaWindow(Ninja &n) : Observer(&n), N(n) {}
+    virtual ~NinjaWindow() {}
+    virtual void draw() { N.draw(); }
+    virtual void update() { draw(); }
+};
 int main()
 {
-    Speed speed;
-    
-    DigitalMeter digitalMeter(&speed);
-    AnalogMeter analogMeter(&speed);
+    Ninja h("Henk"), j("Joop");
+    NinjaWindow hW(h), jW(j);
 
-    Car car(&speed);
-    car.measureSpeed();
+    h.incLevens();
+    h.incLevens();
+    j.incLevens();
+    j.incLevens();
+    j.incLevens();
+    h.incLevens();
     return 0;
 }
