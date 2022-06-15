70fa2dceac229552692f1d3b8a1f951dafc7b655
Wouter de Bruijn <wouter@hedium.nl>
2022-06-15 14:57:43 +0200

    :poop: Working on design

---
+++ inc/Observer.h
@@ -0,0 +1,33 @@
+#ifndef __Observer_H
+#define __Observer_H
+#include <list>
+using namespace std;
+class Subject;
+class Observer
+{
+private:
+  Subject *S;
+
+protected:
+  Subject *getSubject() const { return S; }
+
+public:
+  Observer(Subject *s);
+  virtual ~Observer();
+  virtual void update() = 0;
+};
+class Subject
+{
+private:
+  list<Observer *> L;
+
+protected:
+  virtual void notify();
+
+public:
+  Subject() {}
+  virtual ~Subject() {}
+  virtual void insert(Observer *s) { L.push_front(s); }
+  virtual void remove(Observer *s) { L.remove(s); }
+};
+#endif
